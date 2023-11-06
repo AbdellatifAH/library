82c7ef3df52ad7c952647c58530f7ab6c36cf89d
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-11-07 01:54:44 +0200

    Added Add new book button which brings up a form that takes the info for the new book to be added. Added the form html. Added and Modified some styling of the page.

--- jvs.js
+++ jvs.js
@@ -1,4 +1,12 @@
 const myLibrary = [];
+const form = document.querySelector("form");
+const showButton = document.querySelector("#addBook");
+const favDialog = document.querySelector("#favDialog");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+const title = favDialog.querySelector("#title");
+const author = favDialog.querySelector("#author");
+const pages = favDialog.querySelector("#pages");
+const read = favDialog.querySelector("#read");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -8,26 +16,18 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function () {
-    if (this.read)
-        return `${this.title} by ${this.author}, ${this.pages}, read`
-    else
-        return `${this.title} by ${this.author}, ${this.pages}, not read yet`
-
+    return `<span class="bold">Book Title:</span> ${this.title}<br><span class="bold">Author:</span> ${this.author}<br><span class="bold">Number of Pages:</span> ${this.pages}<br><span class="bold">Read:</span> ${this.read}`
 }
 
-function addBookToLibrary() {
-    title = prompt('Enter Book Title');
-    author = prompt('Enter Book Author');
-    pages = prompt('Enter Book Page Numbers');
-    read = prompt('Have You Read The Book ?');
+function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read))
 }
 
-myLibrary.push(new Book("The Hobbit", "J.R.R", "255", false));
-myLibrary.push(new Book("The Lord Of The Rings", "J.R.R", "655", false));
-myLibrary.push(new Book("A Game Of Thrones", "George R.R. Martin", "698", true));
-myLibrary.push(new Book("A Clash OF Kings", "George R.R. Martin", "698", true));
-myLibrary.push(new Book("A Song Of Ice And Fire", "George R.R. Martin", "698", true));
+myLibrary.push(new Book("The Hobbit", "J.R.R", "255", "No"));
+myLibrary.push(new Book("The Lord Of The Rings", "J.R.R", "655", "No"));
+myLibrary.push(new Book("A Game Of Thrones", "George R.R. Martin", "698", "Yes"));
+myLibrary.push(new Book("A Clash OF Kings", "George R.R. Martin", "698", "Yes"));
+myLibrary.push(new Book("A Song Of Ice And Fire", "George R.R. Martin", "698", "Yes"));
 
 function displayBooks() {
 
@@ -35,9 +35,30 @@ function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         const card = document.createElement("div");
         card.classList.add('card');
-        card.innerText = myLibrary[i].info();
+        card.innerHTML = myLibrary[i].info();
         container.append(card);
     }
 }
+
+function displayBook() {
+
+    const container = document.querySelector(".container");
+    const card = document.createElement("div");
+    card.classList.add('card');
+    card.innerHTML = myLibrary[myLibrary.length - 1].info();
+    container.append(card);
+}
+
+showButton.addEventListener("click", () => {
+    favDialog.showModal();
+});
+
+confirmBtn.addEventListener("click", (event) => {
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    displayBook();
+    event.preventDefault();
+    favDialog.close();
+    form.reset();
+});
 
 displayBooks();
