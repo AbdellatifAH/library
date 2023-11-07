87275efb45918486c45a1e0793b0170c4ba48c60
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-11-07 15:11:32 +0200

    Added a button on the card to change the Read Status of the book

--- jvs.js
+++ jvs.js
@@ -19,6 +19,13 @@ Book.prototype.info = function () {
     return `<span class="bold">Book Title:</span> ${this.title}<br><span class="bold">Author:</span> ${this.author}<br><span class="bold">Number of Pages:</span> ${this.pages}<br><span class="bold">Read:</span> ${this.read}`
 }
 
+Book.prototype.changeReadStatus = function () {
+    if (this.read == "Yes")
+        this.read = "No";
+    else if (this.read == "No")
+        this.read = "Yes"
+}
+
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read))
 }
@@ -34,14 +41,23 @@ function displayBooks() {
     const container = document.querySelector(".container");
     myLibrary.forEach((book) => {
         const card = document.createElement("div");
+        const display = document.createElement("div");
         const deleteBtn = document.createElement("button");
+        const readBtn = document.createElement("button");
+        readBtn.innerText = "Read";
+        readBtn.addEventListener('click', () => {
+            book.changeReadStatus();
+            display.innerHTML = book.info();
+        });
         deleteBtn.innerText = "Delete Book";
         deleteBtn.dataset.title = book.title;
         deleteBtn.addEventListener('click', deleteBook);
         card.classList.add('card');
-        card.innerHTML = book.info();
+        display.innerHTML = book.info();
         container.append(card);
+        card.append(display);
         card.append(deleteBtn);
+        card.append(readBtn);
     })
 }
 
@@ -50,15 +66,25 @@ function displayBook() {
 
     const container = document.querySelector(".container");
     const card = document.createElement("div");
+    const display = document.createElement("div");
     const deleteBtn = document.createElement("button");
+    const readBtn = document.createElement("button");
     const index = myLibrary.length - 1;
+    readBtn.innerText = "Read";
+    readBtn.addEventListener('click', () => {
+        myLibrary[index].changeReadStatus();
+        display.innerHTML = myLibrary[index].info();
+    });
     deleteBtn.innerText = "Delete Book";
     deleteBtn.dataset.title = myLibrary[index].title;
     deleteBtn.addEventListener('click', deleteBook);
     card.classList.add('card');
-    card.innerHTML = myLibrary[index].info();
+    display.innerHTML = myLibrary[index].info();
     container.append(card);
+    card.append(display);
     card.append(deleteBtn);
+    card.append(readBtn);
+
 }
 
 function deleteBook() {
