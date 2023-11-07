5871517fae89af2915af17e25634aba39d60021f
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-11-07 13:45:02 +0200

    Added a delete book button on each card to delete the book

--- jvs.js
+++ jvs.js
@@ -23,42 +23,62 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read))
 }
 
-myLibrary.push(new Book("The Hobbit", "J.R.R", "255", "No"));
-myLibrary.push(new Book("The Lord Of The Rings", "J.R.R", "655", "No"));
-myLibrary.push(new Book("A Game Of Thrones", "George R.R. Martin", "698", "Yes"));
-myLibrary.push(new Book("A Clash OF Kings", "George R.R. Martin", "698", "Yes"));
-myLibrary.push(new Book("A Song Of Ice And Fire", "George R.R. Martin", "698", "Yes"));
+addBookToLibrary("The Hobbit", "J.R.R", "255", "No");
+addBookToLibrary("The Lord Of The Rings", "J.R.R", "655", "No");
+addBookToLibrary("A Game Of Thrones", "George R.R. Martin", "698", "Yes");
+addBookToLibrary("A Clash OF Kings", "George R.R. Martin", "698", "Yes");
+addBookToLibrary("A Song Of Ice And Fire", "George R.R. Martin", "698", "Yes");
 
 function displayBooks() {
 
     const container = document.querySelector(".container");
-    for (let i = 0; i < myLibrary.length; i++) {
+    myLibrary.forEach((book) => {
         const card = document.createElement("div");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete Book";
+        deleteBtn.dataset.title = book.title;
+        deleteBtn.addEventListener('click', deleteBook);
         card.classList.add('card');
-        card.innerHTML = myLibrary[i].info();
+        card.innerHTML = book.info();
         container.append(card);
-    }
+        card.append(deleteBtn);
+    })
 }
+
 
 function displayBook() {
 
     const container = document.querySelector(".container");
     const card = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+    const index = myLibrary.length - 1;
+    deleteBtn.innerText = "Delete Book";
+    deleteBtn.dataset.title = myLibrary[index].title;
+    deleteBtn.addEventListener('click', deleteBook);
     card.classList.add('card');
-    card.innerHTML = myLibrary[myLibrary.length - 1].info();
+    card.innerHTML = myLibrary[index].info();
     container.append(card);
+    card.append(deleteBtn);
+}
+
+function deleteBook() {
+
+    myLibrary.splice(this.dataset.title, 1);
+    this.parentElement.remove();
+
 }
 
 showButton.addEventListener("click", () => {
     favDialog.showModal();
 });
 
-confirmBtn.addEventListener("click", (event) => {
+confirmBtn.addEventListener("click", (e) => {
     addBookToLibrary(title.value, author.value, pages.value, read.value);
     displayBook();
-    event.preventDefault();
+    e.preventDefault();
     favDialog.close();
     form.reset();
 });
+
 
 displayBooks();
