9c26beac6cf30d19358965526d2628ed74e8b8e3
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-11-18 20:53:14 +0200

    Used Classes to create Objects, instead of objects constructors.

--- jvs.js
+++ jvs.js
@@ -8,37 +8,39 @@ const author = favDialog.querySelector("#author");
 const pages = favDialog.querySelector("#pages");
 const read = favDialog.querySelector("#read");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.info = function () {
-    return `<span class="bold">Book Title:</span> ${this.title}<br><span class="bold">Author:</span> ${this.author}<br><span class="bold">Number of Pages:</span> ${this.pages}<br><span class="bold">Read:</span> ${this.read}`
-}
+    info() {
+        return `<span class="bold">Book Title:</span> ${this.title}<br><span class="bold">Author:</span> ${this.author}<br><span class="bold">Number of Pages:</span> ${this.pages}<br><span class="bold">Read:</span> ${this.read}`
+    }
 
-Book.prototype.changeReadStatus = function () {
-    if (this.read == "Yes")
-        this.read = "No";
-    else if (this.read == "No")
-        this.read = "Yes"
+    changeReadStatus() {
+        if (this.read == "Yes")
+            this.read = "No";
+        else if (this.read == "No")
+            this.read = "Yes"
+    }
 }
-
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read))
 }
 
-addBookToLibrary("The Hobbit", "J.R.R", "255", "No");
-addBookToLibrary("The Lord Of The Rings", "J.R.R", "655", "No");
-addBookToLibrary("A Game Of Thrones", "George R.R. Martin", "698", "Yes");
-addBookToLibrary("A Clash OF Kings", "George R.R. Martin", "698", "Yes");
-addBookToLibrary("A Song Of Ice And Fire", "George R.R. Martin", "698", "Yes");
+// addBookToLibrary("The Hobbit", "J.R.R", "255", "No");
+// addBookToLibrary("The Lord Of The Rings", "J.R.R", "655", "No");
+// addBookToLibrary("A Game Of Thrones", "George R.R. Martin", "698", "Yes");
+// addBookToLibrary("A Clash OF Kings", "George R.R. Martin", "698", "Yes");
+// addBookToLibrary("A Song Of Ice And Fire", "George R.R. Martin", "698", "Yes");
 
 function displayBooks() {
 
     const container = document.querySelector(".container");
+    container.textContent = '';
     myLibrary.forEach((book) => {
         const card = document.createElement("div");
         const display = document.createElement("div");
@@ -61,32 +63,6 @@ function displayBooks() {
     })
 }
 
-
-function displayBook() {
-
-    const container = document.querySelector(".container");
-    const card = document.createElement("div");
-    const display = document.createElement("div");
-    const deleteBtn = document.createElement("button");
-    const readBtn = document.createElement("button");
-    const index = myLibrary.length - 1;
-    readBtn.innerText = "Read";
-    readBtn.addEventListener('click', () => {
-        myLibrary[index].changeReadStatus();
-        display.innerHTML = myLibrary[index].info();
-    });
-    deleteBtn.innerText = "Delete Book";
-    deleteBtn.dataset.title = myLibrary[index].title;
-    deleteBtn.addEventListener('click', deleteBook);
-    card.classList.add('card');
-    display.innerHTML = myLibrary[index].info();
-    container.append(card);
-    card.append(display);
-    card.append(deleteBtn);
-    card.append(readBtn);
-
-}
-
 function deleteBook() {
 
     myLibrary.splice(this.dataset.title, 1);
@@ -100,7 +76,7 @@ showButton.addEventListener("click", () => {
 
 confirmBtn.addEventListener("click", (e) => {
     addBookToLibrary(title.value, author.value, pages.value, read.value);
-    displayBook();
+    displayBooks();
     e.preventDefault();
     favDialog.close();
     form.reset();
